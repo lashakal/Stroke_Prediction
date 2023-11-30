fd079515a0dede8f4a33eebac6b8dfc6253b9159
Amadin Ahmed <48179347+amadinahmed@users.noreply.github.com>
2023-11-30 00:09:07 +0000

    push updated Ui

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 
 st.title('Stroke Prediction  :heart:')
 st.caption('Created by:   Amadin Ahmed   Lasha Kaliashvili   Mikheil Uglava')
@@ -9,54 +8,57 @@ st.text("")
 
 
 
-# Define a function to make predictions based on heart data
-def predict_heart_risk(gender, age, hypertension, heart_disease, ever_married, work_type, Residence_type, avg_glucose_level, bmi, smoking_status):
-    # Define the column names based on the heart data features
-    columns = ['gender', 'age', 'hypertension', 'heart_disease', 'ever_married', 'work_type', 'Residence_type', 'avg_glucose_level', 'bmi', 'smoking_status']
-    
-    # Create a DataFrame for the input data
-    data = pd.DataFrame([[gender, age, hypertension, heart_disease, ever_married, work_type, Residence_type, avg_glucose_level, bmi, smoking_status]], columns=columns)
-    
-    # Use the model to make a prediction
-    prediction = model.predict(data)[0]
-    return prediction, data.iloc[0].tolist()
+def predict_stroke_risk(gender, age, hypertension, heart_disease, ever_married, work_type, Residence_type, avg_glucose_level, bmi, smoking_status):
+    # Map the string inputs to their corresponding numerical values
+    gender_num = 0 if gender == "Female" else 1
+    hypertension_num = 0 if hypertension == "No" else 1
+    heart_disease_num = 0 if heart_disease == "No" else 1
+    ever_married_num = 0 if ever_married == "No" else 1
 
-# Helper function to convert numerical data to human-readable form
-def display_readable_form(user_input):
-    gender_text = "Female" if user_input[0] == 0 else "Male"
-    hypertension_text = "No" if user_input[2] == 0 else "Yes"
-    heart_disease_text = "No" if user_input[3] == 0 else "Yes"
-    ever_married_text = "No" if user_input[4] == 0 else "Yes"
-    work_type_text = ["Govt_job", "Never_worked", "Private", "Self_employed", "Children"][user_input[5]]
-    Residence_type_text = "Rural" if user_input[6] == 0 else "Urban"
-    smoking_status_text = ["Unknown", "Formerly Smoked", "Never Smoked", "Smokes"][user_input[9]]
-    
-    return f"Gender: {gender_text}, Age: {user_input[1]}, Hypertension: {hypertension_text}, Heart Disease: {heart_disease_text}, Ever Married: {ever_married_text}, Work Type: {work_type_text}, Residence Type: {Residence_type_text}, Avg Glucose Level: {user_input[7]}, BMI: {user_input[8]}, Smoking Status: {smoking_status_text}"
+    work_type_map = {"Govt_job": 0, "Never_worked": 1, "Private": 2, "Self_employed": 3, "Takes Care of Childern": 4}
+    work_type_num = work_type_map[work_type]
+
+    Residence_type_num = 0 if Residence_type == "Rural" else 1
+
+    smoking_status_map = {"unknown": 0, "formerly smoked": 1, "never smoked": 2, "smokes": 3}
+    smoking_status_num = smoking_status_map[smoking_status]
+
+    # Create a 2D array (or list) for the input data
+    user_data = [gender_num, age, hypertension_num, heart_disease_num, ever_married_num, work_type_num, Residence_type_num, avg_glucose_level, bmi, smoking_status_num]
+
+    # Use the model to make a prediction
+    return user_data[0], user_data
+
+
 
 # User interface in Streamlit to collect user inputs
-gender = st.radio("Gender", (0, 1))  # 0: Female, 1: Male
+gender = st.radio("Gender", ("Female", "Male"))  # 0: Female, 1: Male
 age = st.number_input("Age", min_value=0)
-hypertension = st.radio("Hypertension", (0, 1))  # 0: No, 1: Yes
-heart_disease = st.radio("Heart Disease", (0, 1))  # 0: No, 1: Yes
-ever_married = st.radio("Ever Married", (0, 1))  # 0: No, 1: Yes
-work_type = st.selectbox("Work Type", (0, 1, 2, 3, 4))  # 0: Govt_job, 1: Never_worked, 2: Private, 3: Self_employed, 4: children
-Residence_type = st.radio("Residence Type", (0, 1))  # 0: Rural, 1: Urban
+hypertension = st.radio("Hypertension", ("No", "Yes"))  # 0: No, 1: Yes
+heart_disease = st.radio("Heart Disease", ("No", "Yes"))  # 0: No, 1: Yes
+ever_married = st.radio("Ever Married", ("No", "Yes"))  # 0: No, 1: Yes
+work_type = st.selectbox("Work Type", ("Govt_job", "Never_worked", "Private", "Self_employed", "Takes Care of Childern"))  # 0: Govt_job, 1: Never_worked, 2: Private, 3: Self_employed, 4: children
+Residence_type = st.radio("Residence Type", ("Rural", "Urban"))  # 0: Rural, 1: Urban
 avg_glucose_level = st.number_input("Average Glucose Level", min_value=0.0)
 bmi = st.number_input("BMI", min_value=0.0)
-smoking_status = st.selectbox("Smoking Status", (0, 1, 2, 3))  # 0: unknown, 1: formerly smoked, 2: never smoked, 3: smokes
+smoking_status = st.selectbox("Smoking Status", ("unknown", "formerly smoked", "never smoked", "smokes"))  # 0: unknown, 1: formerly smoked, 2: never smoked, 3: smokes
 
+
+print(gender, age, hypertension, heart_disease, ever_married, work_type, Residence_type, avg_glucose_level, bmi, smoking_status)
+ 
 # Button to make prediction
-if st.button('Predict Heart Risk'):
-    prediction, user_input = predict_heart_risk(gender, age, hypertension, heart_disease, ever_married, work_type, Residence_type, avg_glucose_level, bmi, smoking_status)
+if st.button('Predict Stroke Risk'):
+    prediction, user_input = predict_stroke_risk(gender, age, hypertension, heart_disease, ever_married, work_type, Residence_type, avg_glucose_level, bmi, smoking_status)
     
     # Display user input in readable form
-    st.write("Your input values are:")
-    st.write(display_readable_form(user_input))
+    st.success(user_input)
+    
     
     # Display prediction
-    if prediction == 1:
-        st.error('High risk of heart-related health issues.')
-    elif prediction == 0:
-        st.success('Low risk of heart-related health issues.')
+    #if prediction == 1:
+    #    st.error('High risk of stroke-related health issues.')
+    #elif prediction == 0:
+    #    st.success('Low risk of stroke-related health issues.')
 
-# Rest of your Streamlit code goes here
+
+# i need to import the modal then Run predictions
